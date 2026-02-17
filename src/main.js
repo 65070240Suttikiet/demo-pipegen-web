6d5da5a367f4461aa3d12e9112216b5fda24bdeb
Suttikiet Hattayasomboon <65070240@kmitl.ac.th>
2026-02-17 14:46:13 +0700

    Initial commit with Dockerfile

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+
+    <h1>Hello My name is Mantou kaa!!</h1>
+    <div class="mantou">
+    <img
+      src="https://scontent.fbkk6-2.fna.fbcdn.net/v/t39.30808-6/607066678_122096332623199836_3316483764289744902_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xab0vM6THcoQ7kNvwH77JKF&_nc_oc=AdncK4WO0-v0JZtwHCZ8PCaASHT5B71nzoxuDTPbltZ0SisJOsJ4wVRYE-OyB9fuVf8&_nc_zt=23&_nc_ht=scontent.fbkk6-2.fna&_nc_gid=GseHmq7fx7n7VbSgVz9LSQ&oh=00_Afuw_rKfEJEyCfza4lEkj5BqWuak96uaS_i3amuhKrfaMA&oe=6999DEEC"
+      alt="mantou"
+    />
+    </div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
