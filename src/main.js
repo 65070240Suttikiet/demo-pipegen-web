c9a41f560c4d11d0f772274f202410aa74a4737c
Suttikiet Hattayasomboon <65070240@kmitl.ac.th>
2026-02-18 23:33:14 +0700

    new

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import { setupCounter } from "./counter.js";
 document.querySelector("#app").innerHTML = `
   <div>
 
-    <h1>Hello My name is Mantou kaa!!</h1>
-    <div class="mantou">
+    <h1>Hello Mannnnnnnnnnnnnnnnnnnnnnnnnnnn!!</h1>
+    <div class="man">
     <img
-      src="https://scontent.fbkk6-2.fna.fbcdn.net/v/t39.30808-6/607066678_122096332623199836_3316483764289744902_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xab0vM6THcoQ7kNvwH77JKF&_nc_oc=AdncK4WO0-v0JZtwHCZ8PCaASHT5B71nzoxuDTPbltZ0SisJOsJ4wVRYE-OyB9fuVf8&_nc_zt=23&_nc_ht=scontent.fbkk6-2.fna&_nc_gid=GseHmq7fx7n7VbSgVz9LSQ&oh=00_Afuw_rKfEJEyCfza4lEkj5BqWuak96uaS_i3amuhKrfaMA&oe=6999DEEC"
-      alt="mantou"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf0wybwYIhRAD4u8iBeD029wwYx5Um2_DIyw&s"
+      alt="man"
     />
     </div>
     <div class="card">
